6335face6008cf39ed71453b704f3479637ff855
Wesley <wesley.silva@muralis.com.br>
2021-02-14 17:20:12 -0300

    add Springboot startup class

---
+++ src/main/java/br/com/stoomtest/address/AddressApplication.java
@@ -0,0 +1,13 @@
+package br.com.stoomtest.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressApplication.class, args);
+	}
+
+}
